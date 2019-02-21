26325d24d00e43d92474b3a4c878fbf2b06bd942
Michael Tichý <st52567@student.upce.cz>
2019-02-21 19:09:18 +0100

    commit

---
+++ Cv01/Cv01.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cv01._3
+{
+    class Program
+    {
+        static string GetGender(string id)
+        {
+            int month = int.Parse(id.Substring(2, 2));
+            if (month - 50 > 0)
+            {
+                return "Žena";
+            }
+            else
+            {
+                return "Muž";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string id = "906212/1234";
+
+            Console.WriteLine(GetGender(id));
+        }
+
+    }
+}
---
+++ Cv01/Cv01.4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cv01._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(0, 100);
+            int userNumber;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Zadejte číslo hledané číslo: ");
+                
+                if (!int.TryParse(Console.ReadLine(), out userNumber))
+                {
+                    Console.WriteLine("není číslo!");
+                    i--;
+                    continue;
+                }
+
+                if (userNumber == number)
+                {
+                    Console.WriteLine("Výhra!");
+                    break;
+                }
+                else
+                {
+                    if (number < userNumber)
+                    {
+                        Console.WriteLine("Špatně! Hledané číslo je menší!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Špatně! Hledané číslo je větší!");
+                    }
+                    if (i == 9)
+                    {
+                        Console.WriteLine("Prohra!");
+                    }
+                }
+            }
+        }
+    }
+}
