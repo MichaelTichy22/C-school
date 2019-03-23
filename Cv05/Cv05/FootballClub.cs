30550c5132478ea7f8c95e3614d8d8ffd0cca2eb
Michael Tichý <st52567@student.upce.cz>
2019-03-23 21:17:45 +0100

    Cv05

---
+++ Cv05/Cv05/FootballClub.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cv05
+{
+    public enum FootballClub
+    {
+        None, FC_Porto, Arsenal, Real_Madrid, Chelsea, Barcelona
+    }
+}
---
+++ Cv05/Cv05/FootballClubInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cv05
+{
+    static class FootballClubInfo
+    {
+        static int Count { get; }
+    }
+}
