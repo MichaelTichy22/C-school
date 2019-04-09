cf7450d952abde40ec399e1bf642e08e192c9eb5
Michael Tichý <st52567@student.upce.cz>
2019-04-09 20:35:58 +0200

    Semestrální práce

---
+++ Tichy-SemestralniPrace/Tichy-SemestralniPrace/GameObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tichy_SemestralniPrace
+{
+    abstract class GameObject
+    {
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        public int Size { get; set; }
+        public PlayerColor Owner { get; set; }
+        public bool isFinished { get; set; }
+
+        public abstract void Refresh(int time);
+    }
+}
---
+++ Tichy-SemestralniPrace/Tichy-SemestralniPrace/Planet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tichy_SemestralniPrace
+{
+    class Planet : GameObject
+    {
+        public int UnitCount { get; set; }
+
+        const int BORDER_POINT_COUNT = 32;
+        const double CONTACT_POINTS_CONST = 1.0;
+        const double ORBIT_POINTS_CONST = 1.4;
+
+        public override void Refresh(int time)
+        {
+            if (Owner == PlayerColor.GREY)
+            {
+                return;
+            }
+
+            if (UnitCount == BORDER_POINT_COUNT)
+            {
+                Size = Size * 3;
+            }
+
+        }
+    }
+}
